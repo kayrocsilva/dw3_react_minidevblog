35e13743878db663c51e1321fabdc39ef376d0d1
kayrocsilva <kayro.silva@fatec.sp.gov.br>
2024-03-19 12:18:11 -0300

    Aula1903 - Hooks e Listas Arrays

--- basic/basictools/src/App.jsx
+++ basic/basictools/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import NavBar from './components/NavBar/NavBar'
 import Footer from './components/Footer/Footer'
+import Form from './components/Form/Form'
+import FormHooks from './components/Form/Hooks/FormHooks'
 function App() {
   return (
     <>
     <NavBar></NavBar>
+    <Form></Form>
     <Footer></Footer>
+    <FormHooks></FormHooks>
     </>
   )
 }
---
+++ basic/basictools/src/components/Form/Form.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+var teste =8
+const Form = () => {
+    const handleMyEvent = (e) =>{
+        console.log(e)
+        console.log("Ativou o Evento")
+    }
+const renderButton = (x) => {
+    if(x){
+        return <h1>Renderizou Bonitinho!</h1>
+    } else {
+        return <h1>Renderizou Lindinho!!!</h1>
+    }
+}
+  return (
+       <>
+    <div>
+        <div>
+            <button onClick={handleMyEvent}>Clique aqui</button>
+        </div>
+        <div>
+            <button onClick={() => {console.log("Yes! Apertou em mim.!!!")}}>Clique em mim também!</button>
+        </div>
+        <div>
+            <button onClick={() => {
+                if(true){
+                    console.log("Isso não deveria acontecer!!!")
+                }
+            }}>Será que você clicaria aqui?</button>
+        </div>
+        <div>
+            {renderButton(true)}
+            {renderButton(false)}
+
+        </div>
+    </div>
+    <div>
+        <strong>Valor:</strong>{teste}
+        <button onClick={() =>{ teste = 12}}> Aperte e mude</button>
+        <strong>Valor:</strong>{teste}
+
+    </div>
+    </>
+  )
+}
+
+export default Form

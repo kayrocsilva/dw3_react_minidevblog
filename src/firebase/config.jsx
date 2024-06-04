3d374e9a4f316dc35f179b956ac9988c6605b7de
kayrocsilva <kayro.silva@fatec.sp.gov.br>
2024-06-03 23:19:08 -0300

    correcao CI/CD

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -3,14 +3,14 @@ import { getAnalytics } from "firebase/analytics"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBeThDmxtkDx25QEgafNoNqBSNkpnASqUc",
-  authDomain: "minidevblogicoma.firebaseapp.com",
-  projectId: "minidevblogicoma",
-  storageBucket: "minidevblogicoma.appspot.com",
-  messagingSenderId: "838985901962",
-  appId: "1:838985901962:web:954bb342a62dedd99aca73",
-  measurementId: "G-TYX9XKQDER"
-}
+  apiKey: "AIzaSyCJmxV7ezLT8OBkiEOnMJNL_PoDkG5BdW4",
+  authDomain: "minideveblogkayrocs.firebaseapp.com",
+  projectId: "minideveblogkayrocs",
+  storageBucket: "minideveblogkayrocs.appspot.com",
+  messagingSenderId: "1037572408861",
+  appId: "1:1037572408861:web:00be18b05a7aaa2a3068f4",
+  measurementId: "G-7SJFY9N6LC"
+};
 
 const app = initializeApp(firebaseConfig)
 const analytics = getAnalytics(app)
--- y
+++ y
@@ -1,9 +1,4 @@
-rules_version = '2';
-
-service cloud.firestore {
-  match /databases/{database}/documents {
-    match /{document=**} {
-      allow read, write: if false;
-    }
-  }
+{
+  "indexes": [],
+  "fieldOverrides": []
 }
